bf68051f86dcbdd7cfa17be632739136d06db747
Mickey <mickeyth59@gmail.com>
2023-11-06 22:41:46 +0530

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -11,6 +11,7 @@ import { GoogleOAuthProvider } from '@react-oauth/google';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
+  <>
       <React.StrictMode>
       <DarkModeContextProvider>
         <App />
