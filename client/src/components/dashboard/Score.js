a8a7fd9e1445ba903e9828cae0105b8bd8caff9b
Shreeram <shreeramsaran28@gmail.com>
2023-11-20 22:22:30 +0530

    Admin Updated

--- client/src/components/dashboard/Score.js
+++ client/src/components/dashboard/Score.js
@@ -1,11 +1,11 @@
-import React from 'react'
-
+import React from 'react';
+import ResultTable from '../quiz/ResultTable';
 const Score = () => {
   return (
-    <div>
-      score
+    <div style={{ height: '150px', overflow: 'auto' }}>
+      <ResultTable />
     </div>
-  )
-}
+  );
+};
 
-export default Score
+export default Score;
